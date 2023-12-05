c4e131694ebb389369df95c1cffe1f640b0df29b
Argho Maitra <orgomoit@proton.me>
2023-12-05 23:17:27 +0530

    Chapter 2: Finish exercise

---
+++ ch02/ex2-01.cpp
@@ -0,0 +1,13 @@
+// Chapter 2 - Exercise 1
+
+// This program outputs a message to the monitor
+
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello, programming!\n";	// output "Hello, programming!"
+    cout << "Here we go!\n";		// output "Here we go!"
+    keep_window_open();
+    return 0;
+}
